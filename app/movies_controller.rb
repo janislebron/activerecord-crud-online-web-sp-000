8ca180363d1686680cd822fc0befdc8126293f8d
Janis Lebron <janislebron@gmail.com>
2021-04-21 01:15:59 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,18 +23,19 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = title, release_date)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = "Yea"
+    m.title = "Home Alone"
+    m.release_date = "1990"
   end
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first.title
+  Movie.first.titleize
 end
 
 def can_get_the_last_item_in_the_database
